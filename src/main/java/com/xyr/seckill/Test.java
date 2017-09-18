4147c0d56af3479d6c1c2bb2a3bd3eb65edd00cc
xyr <736845367@qq.com>
2017-09-18 11:00:33 +0800

    Initial commit

---
+++ src/main/java/com/xyr/seckill/Test.java
@@ -0,0 +1,35 @@
+package com.xyr.seckill;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import redis.clients.jedis.Jedis;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Jedis jedis = new Jedis("localhost");
+		System.out.println(jedis.ping());
+		//字符串的存储
+		//jedis.set("§关山难越§", "此岸ǒ彼岸");
+		//System.out.println(jedis.get("§关山难越§"));
+		//存储列表数据 lpush
+		//jedis.lpush("students", "M  ,");
+		//jedis.lpush("students", "jame");
+		//jedis.lpush("students", "anna");
+		//jedis.lpush("students", "amy");
+		//start  end(包含)  0 1 2
+		/*List<String> list = jedis.lrange("students", 0, 3);
+		for(int i = 0 ;i<list.size();i++){
+			System.out.println(list.get(i));
+		}*/
+		Set<String> keys = jedis.keys("*");
+		Iterator<String> itor = keys.iterator();
+		while(itor.hasNext()){
+			System.out.println(itor.next());
+		}
+		
+		
+	}
+
+}
